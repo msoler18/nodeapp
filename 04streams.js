22be60999770b1cb85f194d6a6049db7a906e96c
Hember Colmenares <hemberfer@gmail.com>
2019-11-21 15:13:59 -0500

    Check stream module node

---
+++ 04streams.js
@@ -0,0 +1,21 @@
+/* Stream */
+'use strict'
+
+var fs = require('fs'),
+	readStream = fs.createReadStream('assets/players.txt'),
+	writeStream = fs.createWriteStream('assets/copy_players.txt')
+
+	readStream.pipe(writeStream);
+
+	readStream.on('data',function(chunk){
+		console.log(
+			'Read:',
+			chunk.length,
+			'characters'
+		)
+	})
+
+	readStream.on('end',function(){
+		console.log('Finish read');
+	})
+
