d33cb751f218710e27ff71cfdda6400a26a8e6d0
Hember Colmenares <hemberfer@gmail.com>
2019-11-21 15:56:35 -0500

    Async function stream process

---
+++ 05streamProcess.js
@@ -0,0 +1,43 @@
+'use strict'
+
+var stdin = process.stdin,
+    stdout = process.stdout,
+    person = {
+      name: null,
+      age:0
+    }
+
+
+const saveAge = (age) => {
+  person.age = age;
+
+  if(person.age >= 18){
+    stdout.write(`${person.name} is adult, your age is: ${person.age} years old.`);
+  }else{
+    stdout.write(`${person.name} is young, your age is: ${person.age} years old.`);
+  }
+  process.exit();
+}
+
+
+const saveName = (name) =>{
+  person.name = name;
+
+  let question = `Hello ${person.name}, how old are you ?`;
+
+  quiz(question,saveAge);
+}
+
+
+const quiz = (question,callback) =>{
+  stdin.resume();
+  stdout.write( question + ': ')
+
+  stdin.once('data',function(res){
+    callback( res.toString().trim() );
+  });
+}
+
+
+stdin.setEncoding('utf8');
+quiz('What is your name?', saveName);
